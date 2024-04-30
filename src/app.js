e89da933a90d68f5bc566a6c9fc66ebdf3178d2b
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-04-30 03:39:52 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        console.log(`${pronoun}${adj}${noun}.com`);
+      }
+    }
+  }
 };
